4147991e1e7db9140b963ad5be1fb780a2425f61
ying1488 <yingecteach@gmail.com>
2023-10-27 22:31:10 +1100

    .

---
+++ src/store/slices/usersSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
+
+
+const usersSlice = createSlice({
+    name: 'users',
+    initialState: {
+        data: [],
+        isLoading: false,
+        error: null
+    },
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.pending, (state, acttion) => {
+            //Update our state object , show user hat we are loading data
+            state.isLoading = true;
+        });
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.data = action.payload;
+        });
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error;
+
+        });
+    }
+});
+
+export const usersReducer = usersSlice.reducer;
